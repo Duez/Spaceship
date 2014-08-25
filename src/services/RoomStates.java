56fe1cefba2ecfe98a7e668c71fee6b00cbe26e4
yoann-dufresne <yoann.dufresne0@gmail.com>
2014-08-25 23:23:55 +0200

    Rooms states Service

--- src/services/RoomStates.java
+++ src/services/RoomStates.java
@@ -1,5 +1,43 @@
 package services;
 
-public class RoomStates {
+import events.Event;
+import global.Ship;
+
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
+import rooms.Room;
+
+public class RoomStates implements Service {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public StringBuffer getAnswer(Map<String, String> arg0) {
+		Ship s = Ship.ship;
+		
+		JSONObject answer = new JSONObject();
+		answer.put("oxygen", s.getLife().getOxygenLevel());
+		answer.put("time", s.getEngine().timeToGoal());
+		
+		JSONObject rooms = new JSONObject();
+		for (Room room : s.getAllRooms()) {
+			JSONObject roomJso = new JSONObject();
+			
+			rooms.put(room.getClass().getName(), roomJso);
+			
+			JSONObject eventJso = new JSONObject();
+			if (room.getEvent() != null) {
+				Event e = room.getEvent();
+				eventJso.put("name", e.getClass().getName());
+				eventJso.put("duration", e.getDuration());
+				eventJso.put("start", e.getStartTime());
+			}
+			roomJso.put("event", eventJso);
+		}
+		answer.put("rooms", rooms);
+		
+		return new StringBuffer(answer.toJSONString());
+	}
 
 }
