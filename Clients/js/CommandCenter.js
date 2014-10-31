4b5ff32acb46d717420c76bba6334588d1510dae
Marc <Marc.duez@lifl.fr>
2014-10-31 03:41:37 +0100

    CommandCenter.html : add result/statistic and more

--- Clients/js/CommandCenter.js
+++ Clients/js/CommandCenter.js
@@ -10,6 +10,12 @@ var r2 = 0.44
  * */
 function CommandCenter() {
     console.log("CommandCenter: start")
+    
+    this.isStarted = false;
+    this.mem = {
+        "oxy" : [],
+        "time" : []
+    };
 }
 
 
@@ -21,6 +27,7 @@ CommandCenter.prototype = {
             window.setTimeout(function(){self.init()}, 1000);
             return;
         }
+        
         console.log("CommandCenter: init")
         //var
         this.rooms = []
@@ -40,6 +47,11 @@ CommandCenter.prototype = {
         window.onresize = function () {
             self.resize()
         };
+        
+        //save
+        for (var key in spaceship.data.rooms) {
+            this.mem[key]=[]   
+        }
         
         this.resize();
         this.update();
@@ -153,6 +165,7 @@ CommandCenter.prototype = {
     
     update_goal : function () {
         if ((this.goal.last - spaceship.data.time) > 0){
+            this.start()
             this.goal.value.style.color = "rgba(7,165,210,1)"
         }else{
             this.goal.value.style.color = "rgba(253,0,13,1)"
@@ -181,7 +194,16 @@ CommandCenter.prototype = {
         this.update_event();
         this.update_goal();
         this.update_oxygen();
-        window.setTimeout(function(){self.update()}, 1000);
+        
+        this.save()
+        
+        if (spaceship.data.oxygen == 0) {
+            this.end()
+        }else if (spaceship.data.time == 0) {
+            this.end()
+        } else {
+            window.setTimeout(function(){self.update()}, 1050);
+        }
     },
     
     /* 
@@ -201,6 +223,91 @@ CommandCenter.prototype = {
             }
         }
     },
+    
+    start : function(){
+        if(!this.isStarted){
+            this.isStarted = true
+            this.begin = new Date().getTime()
+        }
+    },
+    
+    end : function() {
+        $("#end").css('display','block')
+        
+        //fail/win
+        if (spaceship.data.oxygen == 0) {
+            $("#result").html("Game Over")
+            $("#result").css("color","rgba(253,0,13,1)")
+        }else{
+            $("#result").html("Victory")
+            $("#result").css("color","rgba(7,165,210,1)")
+        }
+        
+        //game_time
+        var ellapsed_time = ( new Date().getTime() - this.begin ) / 1000
+        var minute = Math.floor(ellapsed_time/60)
+        var sec = Math.floor(ellapsed_time%60)
+        if (sec<10) sec = "0"+sec
+        $("#score").html( minute + ":" + sec )
+        
+        //graph
+        var l = this.mem.oxy.length
+        var width = (100/l)+"%"
+        for (var i=0; i<l; i++){
+            var left = ((100*i)/l)+"%"
+            var height = ((100*this.mem.oxy[i])/this.oxygen.max)+"%"
+            var top = (100-((100*this.mem.oxy[i])/this.oxygen.max))+"%"
+            jQuery('<div/>', {
+                style: 'width : '+width+'; left : '+left+'; height : '+height+'; top : '+top,
+                class: 'oxygen_bar'
+            })
+            .appendTo("#graph")
+            
+            var height = (100-((100*this.mem.time[i])/this.goal.max))+"%"
+            var top = ((100*this.mem.time[i])/this.goal.max)+"%"
+            jQuery('<div/>', {
+                style: 'width : '+width+'; left : '+left+'; height : '+height+'; top : '+top,
+                class: 'time_bar'
+            })
+            .appendTo("#graph")    
+        }
+        
+        //stats
+        for (var key in spaceship.data.rooms) {
+            var div = jQuery('<div/>', {
+                html: rooms_def[key].name,
+                class: 'room_stat'
+            })
+            
+            for (var i=0; i<l; i++){
+                if (this.mem[key][i] != "0"){
+                    var left = ((100*i)/l)+"%"
+                    jQuery('<div/>', {
+                        style: 'width : '+width+'; left : '+left+'; background-color : '+event_def[this.mem[key][i]].color,
+                        class: 'event_bar'
+                    })
+                    .appendTo(div)
+                }
+                
+            }
+            div.appendTo("#stats")
+        }
+        
+    },
+    
+    save : function() {
+        if(this.isStarted){
+            this.mem.oxy.push(spaceship.data.oxygen)
+            this.mem.time.push(spaceship.data.time)
+            for (var key in spaceship.data.rooms) {
+                var e = "0"
+                if(typeof spaceship.data.rooms[key].event != 'undefined'){
+                    e = spaceship.data.rooms[key].event.name
+                }
+                this.mem[key].push(e)
+            }
+        }
+    }  
 
 }//end CommandCenter prototype
 
@@ -239,6 +346,7 @@ function shipProc(p) {
             p.drawSegment(start,stop,isBroken)
             i++
         }
+        
         
     };
     
